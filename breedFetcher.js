09eed59eebb249ae4687b40deaeee08038f45996
jchanpark <junpark4936@gamil.com>
2022-03-17 22:11:43 -0400

    refactored using callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,18 @@
 const request = require('request');
-const args = process.argv.slice(2);
-let breed = args[0].substring(0, 4);
-let url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`error - page not found`);
-  }
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Input beed not found.');
-  }
-  console.log(data[0].breeds);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(`error - page not found`, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      return callback('Input beed not found.', null);
+    }
+    return callback(null, data[0].description);
+  });
+};
 
+module.exports = { fetchBreedDescription };
